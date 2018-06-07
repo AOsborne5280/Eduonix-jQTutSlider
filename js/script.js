a4739cec65cfac789a96c9334cec51d3ea5416cd
aosborne <osbornedesign@gmail.com>
2018-06-06 20:12:30 -0600

    finished js notes

--- js/script.js
+++ js/script.js
@@ -1,17 +1,17 @@
-// Accordian
+// Accordian from Eduonix/Traversy mini tutorial
 var action = "click";
 var speed = "500";
 
 $(document).ready(function () {
-  //Question Handler
+  //Question Handler: when li.q is action-ed(click) run this function
   $('li.q').on(action, function (){
-  //get the next eliment
+    //the element after li.q gets slide toggled
     $(this).next()
       .slideToggle (speed)
-  //select all other answers
+        //and all of its sibling are slid closed
         .siblings('li.a')
           .slideUp ();
-  //get image for active question
+    //the img children of the active li.q are selected
     var img = $(this).children('img');
   //remove rotate class for all images except the active
   $('img').not(img).removeClass('rotate');
